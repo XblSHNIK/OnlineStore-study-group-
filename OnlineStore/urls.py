24cf34debba770caed0443277401363317005e76
XblSHNIK <65400361+XblSHNIK@users.noreply.github.com>
2020-05-25 23:04:26 +0300

    Еще раз

--- OnlineStore/urls.py
+++ OnlineStore/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from OnlineStore import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
+    path('', views.index),
+    path('create/', views.create),
+    path('edit/<int:id>/', views.edit),
+    path('delete/<int:id>/', views.delete),
+
 ]
---
+++ OnlineStore/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponseNotFound
+from .models import Product
+
+def index(request):
+    Goods = Product.objects.all()
+    return render(request, "index.html", {"Goods": Goods})
+def create(request):
+    if request.method == "POST":
+        Product1 = Product()
+        Product1.name = request.POST.get("name")
+        Product1.price = request.POST.get("price")
+        Product1.Description = request.POST.get("Description")
+        Product1.save()
+    return HttpResponseRedirect("/")
+
+
+
+
+def edit(request, id):
+    try:
+        Product1 = Product.objects.get(id=id)
+
+        if request.method == "POST":
+            Product1.name = request.POST.get("name")
+            Product1.price = request.POST.get("price")
+            Product1.Description = request.POST.get("Description")
+            Product1.save()
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, "edit.html", {"Product1": Product})
+    except Product.DoesNotExist:
+        return HttpResponseNotFound("<h2>Товар не найден</h2>")
+
+
+def delete(request, id):
+    try:
+        Product1 = Product.objects.get(id=id)
+        Product1.delete()
+        return HttpResponseRedirect("/")
+    except Product.DoesNotExist:
+        return HttpResponseNotFound("<h2>Товар не найден</h2>")
